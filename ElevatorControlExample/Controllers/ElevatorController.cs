ea785272169a8aa5e087034793accc23a5b2697d
James Lemon <jamesdlemon@gmail.com>
2023-08-16 17:05:01 -0700

    Elevator Call Endpoint outlined

---
+++ ElevatorControlExample/Controllers/ElevatorController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ElevatorControlExample.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ElevatorController : ControllerBase
+    {
+        [HttpPost(Name = "CallElevator")]
+        public bool Post(int floor)
+        {
+            return true;
+        }
+    }
+}
